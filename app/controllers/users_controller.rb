179e096b767adf3741b4391d29589037a3f1af7d
Tyler Pulse <32463199+TylerP33@users.noreply.github.com>
2018-03-23 16:41:49 -0500

    Signup
    Added Signup.erb page to Views
    Added get /signup and post

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,20 @@
 class UsersController < ApplicationController
 
 	get '/signup' do
-		erb :'users/signup'
-	end
+        if !logged_in?
+            erb :"users/signup"
+        else
+            redirect "/freight"
+        end
+    end
+	
+	post '/signup' do
+		if params[:username].empty? || params[:email].empty? || params[:password].empty?
+			redirect "/signup"
+		else 
+			@user = User.create(username: params[:username], email: params[:email], password: params[:password])
+     		session[:user_id] = @user.id
+			redirect "/freight"
+		end
+ 	end
 
-end 
