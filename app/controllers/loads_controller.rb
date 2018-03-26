6f1b793a3087645fc17c609515bd8bdbbe83a37e
Tyler Pulse <32463199+TylerP33@users.noreply.github.com>
2018-03-26 11:40:32 -0500

    /load/new functionality
    Made sure a new load was being created and saved.
    
    Also, added true/false functionality to hazardous materials checkbox

--- app/controllers/loads_controller.rb
+++ app/controllers/loads_controller.rb
@@ -1,13 +1,27 @@
 class LoadsController < ApplicationController
 
 	get "/loads" do
+		redirect_if_not_logged_in
 		@loads = Load.all 
 		erb :"/loads/show"
 	end
 
-	get "loads/new" do
+	get "/loads/new" do
+		redirect_if_not_logged_in 
 		erb :"/loads/new"
 	end 
+
+	post "/loads" do
+		redirect_if_not_logged_in 
+		if 	params[:pallet_count].empty? || params[:weight].empty? || params[:description].empty?
+			params[:haz_mat].nil? ? false : true
+			redirect '/loads/new?error=You must fill in all the blanks'
+		else
+			@new_load = Load.new(pallet_count: params[:pallet_count], weight: params[:weight], description: params[:description], haz_mat: params[:haz_mat])
+			@new_load.save
+			redirect '/loads'
+		end
+	end
 
 
 end 
