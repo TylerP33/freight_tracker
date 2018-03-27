c8dc30de5a70f6bd9ffc6b9259bfbd8e3c52dd1e
Tyler Pulse <32463199+TylerP33@users.noreply.github.com>
2018-03-27 10:50:15 -0500

    Slugify username

--- app/models/carrier.rb
+++ app/models/carrier.rb
@@ -3,4 +3,11 @@ class Carrier < ActiveRecord::Base
 	
 	has_secure_password
 
+	def slug
+		self.username.downcase.strip.gsub(' ', '-').gsub(/[^\w-]/, '')
+	end
+
+	def self.find_by_slug(slug)
+		self.all.find {|username| username.slug == slug }
+	end
 end
