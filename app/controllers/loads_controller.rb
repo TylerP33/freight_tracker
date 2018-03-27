585e446f3cae4b7b49a8150f14aa187d3e535b40
Tyler Pulse <32463199+TylerP33@users.noreply.github.com>
2018-03-27 14:57:31 -0500

    Edit/Delete functionality implemented
    Implemented the ability to edit or delete a certain load

--- app/controllers/loads_controller.rb
+++ app/controllers/loads_controller.rb
@@ -32,6 +32,34 @@ class LoadsController < ApplicationController
 		@load = Load.find params['id']
 		erb :"/loads/show_load"
 	end
-end 
 
+	get '/loads/:id/edit' do
+		redirect_if_not_logged_in
+		@load = Load.find params['id']
+		erb :'/loads/edit'
+	end 
+
+	patch '/loads/:id' do
+		redirect_if_not_logged_in
+		@load = Load.find params['id']
+	if 	params[:pallet_count].empty? || params[:weight].empty? || params[:description].empty?
+		params[:haz_mat].nil? ? false : true
+		redirect "/loads/#{@load.id}/edit"
+	else
+		@load.update(pallet_count: params[:pallet_count], weight: params[:weight], description: params[:description], haz_mat: params[:haz_mat])
+		redirect "/loads/#{@load.id}"
+	end
+  end
+
+  	delete '/loads/:id/delete' do
+  		redirect_if_not_logged_in
+  		@load = Load.find params['id']
+  		if @load.carrier_id == current_user.id
+  			@load.destroy
+  			redirect '/loads'
+  		else
+  			redirect '/loads/new?error=Only the carrier can do this!'
+  		end
+  	end
+end
 
