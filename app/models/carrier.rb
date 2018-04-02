7d34cef9027997c94052765f9ceed84e26c17eba
Tyler Pulse <32463199+TylerP33@users.noreply.github.com>
2018-04-02 15:12:49 -0500

    Removed slugify
    Won't be looking up any usernames, based off of how the platform functions.

--- app/models/carrier.rb
+++ app/models/carrier.rb
@@ -7,12 +7,4 @@ class Carrier < ActiveRecord::Base
 	validates_presence_of :username, :email, :password
 	validates :username, uniqueness: true
 	#make sure to be freshened up on this for interview
-
-	def slug
-		self.username.downcase.strip.gsub(' ', '-').gsub(/[^\w-]/, '')
-	end
-
-	def self.find_by_slug(slug)
-		self.all.find {|username| username.slug == slug }
-	end
 end
